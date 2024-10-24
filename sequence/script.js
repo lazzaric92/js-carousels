64d25fc9770adcb13da10e6816f5bdaa58836646
lazzaric92 <lazzaric_92@hotmail.it>
2024-10-24 23:25:08 +0200

    fixed typos, wip changing sequence scroll behaviour

--- sequence/script.js
+++ sequence/script.js
@@ -14,8 +14,8 @@ document.addEventListener('DOMContentLoaded', function(){
     const carouselWidth = carouselEl.offsetWidth;
     const slideWidth = slideEl.offsetWidth;
     const slidesNumber = 3;  // <-- number of a fully visible slide
-    const width = carouselWidth - (slideWidth * slidesNumber) - (gap * slidesNumber);  // <-- width of the fully visible slides width their margin
-    console.log(carouselWidth, width);
+    const width = carouselWidth - (slideWidth * slidesNumber) - (gap * slidesNumber);  // <-- width of the fully visible slides with their margin
+    // console.log(carouselWidth, width);
 
     nextBtn.addEventListener('click', moveForward);
     prevBtn.addEventListener('click', moveBackward);
@@ -23,59 +23,25 @@ document.addEventListener('DOMContentLoaded', function(){
     
     // ! FUNCTIONS
     function moveForward(){
-        carouselEl.scrollBy(carouselWidth - width, 0);
-        if (carouselEl.scrollWidth !== 0) {
-            prevBtn.style.display = "flex";
-        }
-        if (carouselEl.scrollWidth - width <= carouselEl.scrollLeft + carouselWidth) {
-            nextBtn.style.display = "none";
-        }
+        containerEl.scrollLeft += carouselWidth - width;
+        // containerEl.scrollBy(carouselWidth - width, 0);
+        // console.log("scrollWidth " + carouselEl.scrollWidth, "-- scrollWidth - width " + (carouselEl.scrollWidth - width), "-- scrollLeft + carouselWidth " + (carouselEl.scrollLeft + carouselWidth));
+        // if (carouselEl.scrollWidth !== 0) {
+        //     prevBtn.style.display = "flex";
+        // }
+        // if (carouselEl.scrollWidth - width <= carouselEl.scrollLeft + carouselWidth) {
+        //     nextBtn.style.display = "none";
+        // }
     }
 
     function moveBackward(){
-        carouselEl.scrollBy(-(carouselWidth + gap), 0);
-        if (carouselEl.scrollLeft - carouselWidth - gap <= 0) {
-            prevBtn.style.display = "none";
-        }
-        if (!(carouselEl.scrollWidth - carouselWidth - gap <= carouselEl.scrollLeft + carouselWidth)) {
-            nextBtn.style.display = "flex";
-        }
+        containerEl.scrollLeft += width- carouselWidth;
+        // containerEl.scrollBy(-(carouselWidth), 0);
+        // if (carouselEl.scrollLeft - carouselWidth <= 0) {
+        //     prevBtn.style.display = "none";
+        // }
+        // if (!(carouselEl.scrollWidth - carouselWidth <= carouselEl.scrollLeft + carouselWidth)) {
+        //     nextBtn.style.display = "flex";
+        // }
     }
 });
-
-
-/* 
-
-const gap = 16;
-
-const carousel = document.getElementById("carousel");
-  content = document.getElementById("container");
-  slide = carousel.querySelector('#container > div');
-  next = document.getElementById("next");
-  prev = document.getElementById("prev");
-
-next.addEventListener("click", e => {
-  carousel.scrollBy(width - (width - slideWidth * 5 - gap*5), 0);
-  if (carousel.scrollWidth !== 0) {
-    prev.style.display = "flex";
-  }
-  if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-    next.style.display = "none";
-  }
-});
-prev.addEventListener("click", e => {
-  carousel.scrollBy(-(width + gap), 0);
-  if (carousel.scrollLeft - width - gap <= 0) {
-    prev.style.display = "none";
-  }
-  if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-    next.style.display = "flex";
-  }
-});
-
-let width = carousel.offsetWidth;
-let slideWidth = slide.offsetWidth;
-// window.addEventListener("resize", e => (width = carousel.offsetWidth));
-
-
-*/
