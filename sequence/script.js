ac4c222a7f7bdbfc295a1a8441e74361ca5df74f
lazzaric92 <lazzaric_92@hotmail.it>
2024-10-27 10:35:51 +0100

    fixed sequence slide size

--- sequence/script.js
+++ sequence/script.js
@@ -4,62 +4,104 @@ document.addEventListener('DOMContentLoaded', function(){
     const sequenceEl = document.getElementById('sequence');
     const carouselEl = sequenceEl.querySelector('div.carousel');
     const containerEl = sequenceEl.querySelector('.slides-container');
-    const slides = sequenceEl.querySelectorAll('.slides-container > img');
+    const slides = sequenceEl.querySelectorAll('.slide');
     // buttons to change slide
     const prevBtn = sequenceEl.querySelector('.carousel-arrow.prev');
     const nextBtn = sequenceEl.querySelector('.carousel-arrow.next');
     
     // ! VARIABLES
+    const slidesArray = [
+        {
+            "url": "img/01.png",
+            "alt": "Astro Bot"
+        },
+        {
+            "url": "img/02.jpg",
+            "alt": "Hollow Knight"
+        },
+        {
+            "url": "img/03.jpg",
+            "alt": "Silent Hill 2 Remake"
+        },
+        {
+            "url": "img/04.webp",
+            "alt": "Stray"
+        },
+        {
+            "url": "img/05.png",
+            "alt": "Constance"
+        },
+        {
+            "url": "img/06.jpg",
+            "alt": "Lies of P"
+        },
+        {
+            "url": "img/07.jpg",
+            "alt": "Riven Remake"
+        },
+        {
+            "url": "img/08.avif",
+            "alt": "Dredge"
+        },
+        {
+            "url": "img/09.jpg",
+            "alt": "Bloodborne"
+        },
+        {
+            "url": "img/10.webp",
+            "alt": "Omori"
+        },
+        {
+            "url": "img/11.png",
+            "alt": "Ico"
+        },
+        {
+            "url": "img/12.png",
+            "alt": "Enotria: The Last Song"
+        },
+        {
+            "url": "img/13.webp",
+            "alt": "Cult of the Lamb"
+        },
+        {
+            "url": "img/14.png",
+            "alt": "Undertale"
+        },
+        {
+            "url": "img/15.jpeg",
+            "alt": "Persona 3 Reload"
+        },
+        {
+            "url": "img/16.jpg",
+            "alt": "Stardew Valley"
+        },
+        {
+            "url": "img/17.jpg",
+            "alt": "Inscryption"
+        },
+        {
+            "url": "img/18.jpg",
+            "alt": "Resident Evil Village"
+        },
+        {
+            "url": "img/19.webp",
+            "alt": "Dead Space Remake"
+        },
+        {
+            "url": "img/20.jpg",
+            "alt": "Bayonetta"
+        },
+    ]
     const gap = 16;
     const carouselWidth = carouselEl.offsetWidth;
-    const slideWidth = slides[0].offsetWidth;
-    const slidesNumber = 3;  // <-- number of fully visible slides
-    const width = carouselWidth - (slideWidth * slidesNumber) - (gap * slidesNumber);  // <-- width of the fully visible slides with their margin
-
-    const slideOffLeft = slides[slidesNumber].offsetLeft;
-
     
+    console.log(carouselWidth)
 
-    slides.forEach((slide, index) => {
-        if(index % slidesNumber === 0){
-            console.log(slide.offsetLeft);
-        }
+    prevBtn.addEventListener('click', () => {
+        containerEl.scrollBy(-carouselWidth, 0);
+    })
+
+    nextBtn.addEventListener("click", () => {
+        containerEl.scrollBy(carouselWidth, 0);
     });
-
-
-    // console.log(slides);
-    nextBtn.addEventListener('click', moveForward);
-    prevBtn.addEventListener('click', moveBackward);
-    
-    
-    // ! FUNCTIONS
-    function moveForward(){
-        containerEl.scrollLeft += carouselWidth - width;
-        slides.forEach((slide, index) => {
-            if(index % slidesNumber === 0){
-                console.log(slide.offsetLeft);
-            }
-        });
-    }
-
-    function moveBackward(){
-        containerEl.scrollLeft += width- carouselWidth;
-        slides.forEach((slide, index) => {
-            if(index % slidesNumber === 0){
-                console.log(slide.offsetLeft);
-            }
-        });
-    }
 });
-
-
-/* 
-3n slide offsetLeft
-0 
-905 
-1810 
-2715 
-3621 
-4526 
-5431
-*/
